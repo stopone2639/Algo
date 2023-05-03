1fc1d653195ce2b23f004ce15d7681e8c3538950
stopone2639 <39790068+stopone2639@users.noreply.github.com>
2023-05-03 23:08:49 +0900

    [Silver III] Title: N과 M (4), Time: 208 ms, Memory: 19596 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+        arr = new int[M];
+        DFS(0, 1);
+        System.out.println(sb.toString());
+    }
+    public static void DFS(int r, int before){
+        if(r == M){
+            for(int i = 0; i < M; i++){
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i = 1; i <=N; i++){
+            if(before <= i){
+                arr[r] = i;
+                DFS(r+1, i);
+            }
+        }
+    }
+}
