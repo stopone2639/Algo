f33d122b0d420b0ec2f12b067ad06dd08b8741d0
stopone2639 <39790068+stopone2639@users.noreply.github.com>
2023-05-05 21:34:15 +0900

    [Silver III] Title: N과 M (6), Time: 152 ms, Memory: 15852 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main{
+    static int N, M;
+    static int[] inputArr, arr;
+    static boolean[] visit;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+        inputArr = new int[N];
+        visit = new boolean[N];
+        arr = new int[M];
+        String[] input2 = br.readLine().split(" ");
+        for(int i = 0; i < N; i++){
+            inputArr[i] = Integer.parseInt(input2[i]);
+        }
+        Arrays.sort(inputArr);
+        DFS(0,0);
+        System.out.println(sb.toString());
+    }
+    public static void DFS(int r, int start){
+        if(r == M){
+            for(int i = 0; i < M; i++){
+                sb.append(arr[i] +" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i = start; i < N; i++){
+            if(!visit[i]){
+                arr[r] = inputArr[i];
+                visit[i] = true;
+                DFS(r+1, i + 1);
+                visit[i] = false;
+            }
+        }
+    }
+}
