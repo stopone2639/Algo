5471c248f5be0e89d9e870d31003a41a33c93945
stopone2639 <39790068+stopone2639@users.noreply.github.com>
2023-05-05 22:43:28 +0900

    [Silver III] Title: N과 M (8), Time: 240 ms, Memory: 21852 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * 백트래킹 배열 입력 중복 조합
+ */
+public class Main {
+    static int N, M;
+    static int[] inputArr, arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+        inputArr = new int[N];
+        arr = new int[M];
+        String[] input2 = br.readLine().split(" ");
+        for(int i = 0; i < N; i++){
+            inputArr[i] = Integer.parseInt(input2[i]);
+        }
+        Arrays.sort(inputArr);
+        DFS(0, 0);
+        System.out.println(sb.toString());
+    }
+    public static void DFS(int r, int start){
+        if(r == M){
+            for(int i = 0; i< M; i++){
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i = start; i < N; i++){
+            arr[r] = inputArr[i];
+            DFS(r+1, i);
+        }
+    }
+}
